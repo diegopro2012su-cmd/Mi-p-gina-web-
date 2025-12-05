4ff8c82cfa9846896d00af4435a1b6f151be4653
diegopro2012su-cmd <diegopro2012su@gmail.com>
2025-12-05 01:25:47 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function saludo() {
     alert("¡Hola! Esto funciona gracias a GitHub Pages 😄");
 }
+function saludo() {
+    const nombre = prompt("¿Cómo te llamas?");
+    alert("¡Hola " + nombre + "! Bienvenido a mi web 😎");
+}
